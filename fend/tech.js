829f7fff5b086e3013c7c71af10ef27468b5fabd
spaul-12 <sp.21u10434@btech.nitdgp.ac.in>
2022-05-22 23:07:19 +0530

    signup backend and frontend included

--- fend/tech.js
+++ fend/tech.js
@@ -1,4 +1,4 @@
-function onSignIn(googleUser) {
+/* function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
     console.log("ID: " + profile.getId()); // Don't send this directly to your server!
@@ -11,11 +11,16 @@ function onSignIn(googleUser) {
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
-  }
+  } */
   
   const modal_btn=document.getElementById("sign-up");
   const close_btn=document.getElementById('close_btn');
   const modal=document.getElementById("modal");
+  const submitbtn=document.getElementById("submit_btn");
+  const username=document.getElementById("username");
+  const email=document.getElementById("email");
+  const password=document.getElementById("password");
+
 
   modal_btn.addEventListener('click',()=>{
     console.log(modal);
@@ -26,3 +31,28 @@ function onSignIn(googleUser) {
   close_btn.addEventListener('click',()=>{
     modal.classList.remove('active');
   });
+
+  
+
+  submitbtn.addEventListener('click',submit);
+
+  function submit()
+  {
+     let xhr= new XMLHttpRequest();
+    let url="http://127.0.0.1:3000/api/user/signup";
+
+     xhr.open("POST",url,true);
+     xhr.setRequestHeader("Content-type", "application/json");
+
+     xhr.onreadystatechange= function ()
+     {
+       if(xhr.readyState ===4 && xhr.status===200 )
+       {
+         console.log(this.reponseText);
+       }
+     };
+      var data =JSON.stringify({"email": email.value,"username": username.value, "password":password.value});
+
+      xhr.send(data);
+    
+  }
